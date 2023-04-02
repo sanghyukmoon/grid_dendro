a23c4ff381a9c40c49741e7b84edc36d9c64d640
Sanghyuk Moon <sanghyuk.moon@princeton.edu>
2023-04-01 21:40:24 -0400

    Save ancestors and descendants on-the-fly

--- fhiso/dendrogram.py
+++ fhiso/dendrogram.py
@@ -39,6 +39,7 @@ def construct_dendrogram(arr, boundary_flag='periodic'):
         raise ValueError(f"Boundary flag {boundary_flag} is not supported")
     arr_min_filtered = minimum_filter(arr, size=3, mode=filter_mode)
     leaf_nodes = np.where(arr_flat == arr_min_filtered.flatten())[0]
+
     num_leaves = len(leaf_nodes)
     nodes = {idx: [idx,] for idx in leaf_nodes}
     child_nodes = {idx: set() for idx in leaf_nodes}
@@ -47,6 +48,8 @@ def construct_dendrogram(arr, boundary_flag='periodic'):
     # initially, all the cells have no parent
     parent_node = np.full(num_cells, -1, dtype=int)
     parent_node[leaf_nodes] = leaf_nodes
+    descendant_node = {idx: [idx,] for idx in leaf_nodes}
+    ancestor_node = {idx: idx for idx in leaf_nodes}
 
     # Load neighbor dictionary
     my_neighbors = boundary.precompute_neighbor(arr.shape, boundary_flag,
@@ -60,22 +63,27 @@ def construct_dendrogram(arr, boundary_flag='periodic'):
         neighbors = my_neighbors[idx]
         mask = parent_node[neighbors] != -1
         neighbors = neighbors[mask]
-        parents = find_ancestor(parent_node, neighbors)
-        num_parents = len(parents)
+        parents = parent_node[neighbors]
+        ancestors = set([ancestor_node[prnt] for prnt in parents])
+        num_parents = len(ancestors)
         if num_parents == 0:
             raise ValueError("Should not reach here")
         elif num_parents == 1:
             # This cell is a member of an existing node.
-            prnt = parents.pop()
+            prnt = ancestors.pop()
             parent_node[idx] = prnt
             nodes[prnt].append(idx)
         elif num_parents == 2:
             # This cell is at the critical point, thus becomes a new parent node;
-            parent_node[idx] = idx
             nodes[idx] = [idx,]
-            child_nodes[idx] = parents
+            parent_node[idx] = idx
+            child_nodes[idx] = ancestors
+            ancestor_node[idx] = idx
+            descendant_node[idx] = [idx,]
             for child in child_nodes[idx]:
-                parent_node[child] = idx
+                descendant_node[idx] += descendant_node[child]
+                for descendant in descendant_node[child]:
+                    ancestor_node[descendant] = idx
             nmerge += 1
             num_remaining_nodes = num_leaves - 1 - nmerge
             print("Reaching critical point. number of remaining nodes = {}"
@@ -86,10 +94,3 @@ def construct_dendrogram(arr, boundary_flag='periodic'):
         else:
             raise ValueError("This cell have more than two neighboring parent.")
     return nodes, child_nodes
-
-
-def find_ancestor(parent_node, indices):
-    parents = parent_node[indices]
-    if not np.all(parents == indices):
-        parents = find_ancestor(parent_node, parents)
-    return set(parents)
